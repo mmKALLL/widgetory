60b9522d701cef18182b9b8ed6baa851596c0e7c
Esa Koskinen <esakoskin2@gmail.com>
2020-12-27 14:45:11 +0900

    use array instead of newline-delimited strings for button subtext

--- src/components/action-button/action-button.tsx
+++ src/components/action-button/action-button.tsx
@@ -1,12 +1,20 @@
-import React, { ReactElement } from "react"
+import React, { ReactElement } from 'react'
 import './action-button.sass'
-import Button from "../button/button";
+import Button from '../button/button'
 
-export default function ActionButton(props: { text: string, subtext: string, onClick: () => void }): ReactElement {
+export default function ActionButton(props: {
+  text: string
+  subtext: string[]
+  onClick: () => void
+}): ReactElement {
   return (
     <span className="action-button-container">
       <Button text={props.text} onClick={props.onClick} className="button primary-action-button" />
-      {props.subtext.split('\n').map(item => <span key={item} className="action-button-subtext">{item}</span>)}
+      {props.subtext.map((item) => (
+        <span key={item} className="action-button-subtext">
+          {item}
+        </span>
+      ))}
     </span>
   )
 }
